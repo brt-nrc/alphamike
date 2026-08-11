f1098c955d8674c7f865575b417ce57445869d10
iratecord <enricobarto95@gmail.com>
2026-08-11 13:01:25 +0200

    refactor: change main to use new classes

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
-from amclass import find_structures
-from amfunctions import error_log
-import os
+import json
 import time
+import logging
+import argparse
+from pathlib import Path
 from pprint import pprint
+from alphamike.structure_loader import StructureLoader
+from alphamike.domain_creator import create_domain
+from alphamike.mustang_runner import run_mustang
 
-if os.path.isfile('results.csv'):   #Creates new result file
-    os.remove('results.csv')
+logging.basicConfig(level=logging.DEBUG)
+logger: logging.Logger = logging.getLogger(__name__)
 
-structures = find_structures()      # finds structures to analyze
-i = 0
-s = len(structures)
-for structure in structures:
-    structure.check_create_domain() # checks if all domains are available
-    print("Running MUSTANG on structure ", i, " of ", s, end='\r')
-    try:
-        e = structure.run_mustang(mustang_path='MUSTANG_v3.2.3/bin/')
-    except Exception as err:
-        error_log(structure.barcode)
-        error_log(err)
-    i += 1
-print("Done, results in results.csv")
+parser = argparse.ArgumentParser(description="Run Mustang on a list of structures")
+parser.add_argument("input_file", type=str, help="Path to the input file")
+parser.add_argument("result_folder", type=str, help="Path to the result folder")
+parser.add_argument("aggregate_result_file", type=str, help="Path to the aggregate result file")
+
+
+
+def main(input_file: Path, result_folder: Path, aggregate_result_file: Path) -> None:
+    if aggregate_result_file.is_file():
+        aggregate_result_file.unlink()
+        logging.info(f"Deleted existing aggregate result file: {aggregate_result_file}")
+
+    structure_loader: StructureLoader = StructureLoader(input_file)
+    structure_list: list = structure_loader.load_structures()
+
+    tic1 = time.perf_counter()
+    i = 0
+    for structure in structure_list:
+        for domain in structure.domain_list:
+            logging.info(f"Creating domain {domain} for structure {structure.barcode} ({i+1}/{len(structure_list)})")
+            create_domain(domain)
+        i += 1
+    toc1 = time.perf_counter()
+    logging.info(f"Domains created in {toc1 - tic1:0.4f} seconds")
+
+    tic2 = time.perf_counter()
+    missing_barcodes = {}
+    for structure in structure_list:
+        try:
+            run_mustang(structure, result_folder=result_folder, aggregate_result_file=aggregate_result_file)
+        except Exception as err:
+            logging.error(f"Error running Mustang for structure {structure.barcode}: {err}")
+            missing_barcodes[structure.barcode] = str(err)
+    toc2 = time.perf_counter()
+    logging.info(f"Mustang in {toc2-tic2:0.4f} seconds")
+    logging.warning(f"Missing results: {len(missing_barcodes)}: {json.dumps(missing_barcodes, indent=4)}")
+
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    input_file: Path = Path(args.input_file)
+    result_folder: Path = Path(args.result_folder)
+    aggregate_result_file: Path = Path(args.aggregate_result_file)
+
+    main(input_file, result_folder, aggregate_result_file)
