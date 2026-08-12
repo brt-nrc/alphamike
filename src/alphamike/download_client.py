9374f0056718ab71017ab54e29dc41454f668781
iratecord <enricobarto95@gmail.com>
2026-08-12 13:55:04 +0200

    feat: add DownloadError to DownloadClient.download_list_ftp for symmetrical behavior

--- src/alphamike/download_client.py
+++ src/alphamike/download_client.py
@@ -25,11 +25,14 @@ class DownloadClient:
         """Downloads domain boundaries text file."""
         logger.debug(f"Downloading domain boundaries file.")
         filename: str = settings.ftp_server_filename_domain_boundaries
-        with FTP(settings.ftp_server_host_domain_boundaries) as ftp:      # Open FTP connection
-            ftp.login()                                                   # Anonymous login
-        ftp.cwd(settings.ftp_server_path_domain_boundaries)
-        with open(destination_file_path, 'wb') as fp:
-            ftp.retrbinary('RETR '+filename, fp.write)
+        try:
+            with FTP(settings.ftp_server_host_domain_boundaries) as ftp:      # Open FTP connection
+                ftp.login()                                                   # Anonymous login
+                ftp.cwd(settings.ftp_server_path_domain_boundaries)
+                with open(destination_file_path, 'wb') as fp:
+                    ftp.retrbinary('RETR '+filename, fp.write)
+        except Exception as err:
+            raise DownloadError(f"Error occurred while downloading domain boundaries file: {err}")
         return destination_file_path
 
     @staticmethod
@@ -41,7 +44,7 @@ class DownloadClient:
         download_folder.mkdir(parents=True, exist_ok=True)
         download_filepath: Path = download_folder / pdb_name
 
-        with requests.get(settings.rcsb_server_download_root + pdb_name, stream=True) as r:
+        with requests.get(str(settings.rcsb_server_download_root) + pdb_name, stream=True) as r:
             if not r.status_code == 200:
                 raise DownloadError(r.status_code)
             try:
