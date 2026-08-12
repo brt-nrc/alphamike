792c35079c104ad408dddf55199ac16349711416
iratecord <enricobarto95@gmail.com>
2026-08-12 13:55:30 +0200

    chore: improving type hints

--- src/alphamike/structure_loader.py
+++ src/alphamike/structure_loader.py
@@ -6,16 +6,16 @@ class StructureLoader:
         self.file_path = file_path
 
     def load_structures(self) -> list:
-        structure_list = []
+        structure_list: list[Structure] = []
         with open(self.file_path) as structures:
             for lines in structures:
-                line_content = lines.split()
-                num = int(line_content[0])
+                line_content:list[str] = lines.split()
+                num: int = int(line_content[0])
                 if num > 1:
-                    barcode = line_content[1]
-                    domain_list = []
+                    barcode: str = line_content[1]
+                    domain_list: list[str] = []
                     for i in range(num):
-                        domain = line_content[2 + 3 * i]
+                        domain: str = line_content[2 + 3 * i]
                         domain_list.append(domain.replace(',', ''))
                     structure_list.append(Structure(barcode, domain_list))
         return structure_list
