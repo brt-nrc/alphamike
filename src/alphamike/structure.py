400f77cac83518b64d5ad7fcbabae6ac65d09989
iratecord <enricobarto95@gmail.com>
2026-08-12 13:57:41 +0200

    chore: improving type hints in structure

--- src/alphamike/structure.py
+++ src/alphamike/structure.py
@@ -1,18 +1,17 @@
 from pathlib import Path
-from subprocess import run
 from copy import deepcopy
 
 
 class Structure:
-    def __init__(self, b: str, d: list) -> None:
-        self.barcode = b
-        self.domain_list = deepcopy(d)
+    def __init__(self, barcode: str, domain_list: list) -> None:
+        self.barcode: str = barcode
+        self.domain_list: list[str] = deepcopy(domain_list)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.barcode} -> {self.domain_list}"
 
-    def create_descriptor(self, out: Path = 'desc_tmp', pdb_path: Path = 'pdb/') -> None:
-        pdbfiles = [e + '.pdb' for e in self.domain_list]
+    def create_descriptor(self, out: Path = Path('desc_tmp'), pdb_path: Path = Path('pdb/')) -> None:
+        pdbfiles: list[str] = [e + '.pdb' for e in self.domain_list]
         with open(out, 'w') as desc:
             print('>', pdb_path, file=desc, sep='')
             for e in pdbfiles:
